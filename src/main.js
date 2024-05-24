6ee0afd5456a3a23d47d3cc8cdf346ad4652857b
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-05-24 16:01:05 +0700

    [feat] file vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue' 
 import App from './App.vue'
-import Route from './router/index'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js' 
 
-createApp(App).use(Route).use(bootstrap).mount('#app')
+
+createApp(App).use(bootstrap).mount('#app')
